b02304bdedb6bd603a5407f0fd0f65fbb874d5ec
Konstantin Vedernikov <@email.com>
2026-02-28 21:15:38 +0000

    style

--- 00-hello-world/hello_world.cuh
+++ 00-hello-world/hello_world.cuh
@@ -6,5 +6,4 @@
 #include <cuda_device_runtime_api.h>
 #include <cuda_helpers.h>
 
-void CallHelloWorld() {
-}
+void CallHelloWorld() {}
--- 02-binary-image/binary_image.cuh
+++ 02-binary-image/binary_image.cuh
@@ -2,19 +2,18 @@
 
 #include <cuda_helpers.h>
 
-struct Matrix
-{
-    int * data;
+struct Matrix {
+    int *data;
     size_t N;
     size_t M;
 };
 
 Matrix allocMatrixHost(size_t n, size_t m);
 Matrix allocMatrixDevice(size_t n, size_t m);
-void copyMatrixDeviceToHost(const Matrix & src_matrix, Matrix & dst_matrix);
-void copyMatrixHostToDevice(const Matrix & src_matrix, Matrix & dst_matrix);
-Matrix findNearestOnes(const Matrix & src_matrix);
-void freeMatrixHost(Matrix & src_matrix);
-void freeMatrixDevice(Matrix & src_matrix);
+void copyMatrixDeviceToHost(const Matrix &src_matrix, Matrix &dst_matrix);
+void copyMatrixHostToDevice(const Matrix &src_matrix, Matrix &dst_matrix);
+Matrix findNearestOnes(const Matrix &src_matrix);
+void freeMatrixHost(Matrix &src_matrix);
+void freeMatrixDevice(Matrix &src_matrix);
 
-Matrix solve(const Matrix & src_matrix);
+Matrix solve(const Matrix &src_matrix);
